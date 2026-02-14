e3f679facde9d98c50cf4e8a40558f8e70701448
Mohit <mohitt.tech@gmail.com>
2026-02-15 02:40:55 +0530

    Update organizer.py

--- organizer.py
+++ organizer.py
@@ -3,5 +3,10 @@ import shutil
 
 # folder categories and file extentions
 categories = {
-    "Images" :
+    "Images": [".jpg", ".jpeg", ".png", ".gif"],
+    "Documents": [".pdf", ".docx", ".txt", ".pptx"],
+    "Videos": [".mp4", ".mkv", ".mov"],
+    "Audio": [".mp3", ".wav"],
+    "Archives": [".zip", ".rar", ".tar"],
+    "Scripts": [".py", ".js", ".html", ".css"]
 }
